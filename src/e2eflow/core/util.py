7c50fce0d6e13d410a56c5cc8c7c63308474c50a
Johannes Graeter <johannes.graeter@kit.edu>
2018-11-21 23:15:56 +0100

    Fix downsample issue for odd channel dimensions.

--- src/e2eflow/core/util.py
+++ src/e2eflow/core/util.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from ..ops import downsample as downsample_ops
 
 
 def summarized_placeholder(name, prefix=None, key=tf.GraphKeys.SUMMARIES):
@@ -16,3 +17,11 @@ def resize_area(tensor, like):
 def resize_bilinear(tensor, like):
     _, h, w, _ = tf.unstack(tf.shape(like))
     return tf.stop_gradient(tf.image.resize_bilinear(tensor, [h, w]))
+
+def downsample(tensor, num):
+    _,height, width,_ = tensor.shape.as_list()
+    if height%2==0 and width%2==0:
+        return downsample_ops(tensor, num)
+    else:
+        return tf.image.resize_area(tensor,tf.constant([int(height/num),int(width/num)]))
+	
